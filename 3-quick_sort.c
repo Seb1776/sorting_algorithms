0a2a1ca6529e0a878342c4343c93e9f357e6b889
Jaricapa-holberton <2241@holbertonschool.com>
2021-06-13 17:21:54 -0500

    task 3 v2

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,45 +1,12 @@
 #include "sort.h"
 /**
- * quick_sort_ - function to do 'quick sort' Sorting Algorithm
- * @list: Pointer to double linked list
+ * swap - function to swap elements in an array
+ * @array: Pointer to double linked list
  * @size: Size of the array
+ * @left: The index to change
+ * @right: The index to be changed
  * Return: void
  */
-int partition_func(int *array, int start_num, size_t size);
-void swap(int *array, size_t size, int left, int right);
-
-void quick_sort(int *array, size_t size)
-{
-	size_t start_num = 0;
-	int pivot_num = 0;
-	if (start_num < size)
-	{
-		pivot_num = partition_func(array, start_num, size);
-		quick_sort(array, (pivot_num - 1));
-		quick_sort(array, size);
-	}
-	return;
-		
-}
-int partition_func(int *array, int start_num, size_t size)
-{
-	int temp;
-	size_t i;
-	int pivot_value = array[size];
-	int left = start_num;
-	int right = size;
-
-	for (i = 0; i <= right; i++)
-	{
-		if (array[i] <= pivot_value)
-		{
-            swap(array, size, left, right);
-            left++;
-        }
-	}
-    swap(array, size, left, right);
-	return (left);
-}
 void swap(int *array, size_t size, int left, int right)
 {
 	int temp;
@@ -50,5 +17,46 @@ void swap(int *array, size_t size, int left, int right)
 		array[left] = array[right];
 		array[right] = temp;
 		print_array(array, size);
+	}
+}
+/**
+ * lomuto_partition_func - Lomuto partition scheme function
+ * @array: Pointer to double linked list
+ * @start_num: Initial index
+ * @size: Size of the array
+ * Return: void
+ */
+int lomuto_partition_func(int *array, int start_num, size_t size)
+{
+	size_t i, left = start_num, right = size;
+	int pivot_value = array[size];
+
+	for (i = 0; i <= right; i++)
+	{
+		if (array[i] <= pivot_value)
+		{
+			swap(array, size, left, right);
+			left++;
+		}
+	}
+	swap(array, size, left, right);
+	return (left);
+}
+/**
+ * quick_sort - function to do 'quick sort' Sorting Algorithm
+ * @array: Pointer to array
+ * @size: Size of the array
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	size_t start_num = 0;
+	int pivot_num = 0;
+
+	if (start_num < size)
+	{
+		pivot_num = lomuto_partition_func(array, start_num, size);
+		quick_sort(array, (pivot_num - 1));
+		quick_sort(array, size);
 	}
 }
