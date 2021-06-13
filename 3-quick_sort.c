1747dcdeb4ec953778e2ae21f41ed4fb04c6e28f
Jaricapa-holberton <2241@holbertonschool.com>
2021-06-13 17:54:58 -0500

    task 3 v5

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
  * swap - function to swap elements in an array
- * @array: Pointer to double linked list
+ * @array: Pointer to array
  * @size: Size of the array
  * @left: The index to change
  * @right: The index to be changed
@@ -16,12 +16,13 @@ void swap(int *array, size_t size, int left, int right)
 		temp = array[left];
 		array[left] = array[right];
 		array[right] = temp;
+        /* this prints the change as the task demand */
 		print_array(array, size);
 	}
 }
 /**
  * lomuto_partition_func - Lomuto partition scheme function
- * @array: Pointer to double linked list
+ * @array: Pointer to array
  * @size: Size of the array
  * @start: Initial index
  * @end: Final index
@@ -36,16 +37,18 @@ int lomuto_partition_func(int *array, size_t size, size_t start, size_t end)
 	{
 		if (array[i] <= pivot_value)
 		{
+            /* first changes */
 			swap(array, size, left, right);
 			left++;
 		}
 	}
+    /* last change */
 	swap(array, size, left, right);
 	return (left);
 }
 /**
- * my_quick_sort - function to do 'quick sort' Sorting Algorithm
- * @array: Pointer to double linked list
+ * my_quick_sort - personal function to do the task easy
+ * @array: Pointer to array
  * @size: Size of the array
  * @start: Initial index
  * @end: Final index
@@ -57,6 +60,9 @@ void my_quick_sort(int *array, size_t size, size_t start, size_t end)
 
 	if (start < end)
 	{
+        /* because the Lomuto algorithm require a star and end index */
+        /* this function allows do the same algorithm without the */
+        /* restriction of inputs given by the prototipe's task*/
 		pivot_num = lomuto_partition_func(array, size, start, end);
 		my_quick_sort(array, size, start, pivot_num - 1);
 		my_quick_sort(array, size, pivot_num + 1, end);
